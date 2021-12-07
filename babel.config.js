f8e6f6603887299433b88c2e4e2ac59217c28c9f
Allen Zhang (张涛) <tzhangm@trip.com>
2021-12-07 12:38:39 +0800

    upd

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = function (api) {
     api.cache(true);
     const presets = [];
-    const plugins = [['./lib', {projectId: 85615, dsn: 'http://10.128.13.42:9999/api/v1/coverage/client'}]];
+    const plugins = [['./lib', {projectId: 99999, dsn: 'http://xxx.xxx/api/v1/coverage/client'}]];
     return {
         presets,
         plugins
